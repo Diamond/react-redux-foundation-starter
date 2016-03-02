dbade137249e3a3c0e3f13728f573f143042586b
Brandon Richey <brichey@teladoc.com>
2016-03-02 16:53:27 -0500

    Step 1

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const App = (props) => {
+  return (
+    <div>
+      <h1>Teladoc Member Site</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector("#app")
+)
